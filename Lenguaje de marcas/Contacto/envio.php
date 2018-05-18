f6d0644c4dfbe23f59d778b93a5de1721114b5c8
Claudio <claudiochamorro61@gmail.com>
2018-05-18 19:24:05 +0200

    Subo carpeta contacto con el formulario de enviar al correo ya totalmente correcto e funcionable

---
+++ Lenguaje de marcas/Contacto/envio.php
@@ -0,0 +1,21 @@
+
+<?php
+require("index.html");
+	$destino = "luuudaaa2@gmail.com";
+	$nombre = $_POST["Nombre"];
+	$correo = $_POST["Correo"];
+	$telefono = $_POST["Telefono"];
+	$mensaje = $_POST["mensaje"];
+	$contenido = "Nombre: " . $nombre . "\n Correo: " . $correo . "\n Teléfono: " . $telefono . "\n Mensaje: " . $mensaje;
+	mail($destino, "Contacto", $contenido);
+	header("Location:gracias.html");
+
+
+
+
+
+
+
+
+
+	?>
