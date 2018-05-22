281332df64c68249e67e0a0a4b53dc81e14abbd8
Pepe <pepegascobule99@hotmail.com>
2018-05-22 16:24:22 +0200

    Base de datos acabada

--- Base de datos/Tragaperras.sql
+++ Base de datos/Tragaperras.sql
@@ -51,6 +51,8 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `Pulsaciones`(
 
 ,
 	IN `DN1` VARCHAR(9)
+,
+	IN `Ganado` INT
 )
     COMMENT 'False=0, true=1'
 BEGIN
@@ -59,6 +61,8 @@ UPDATE estadisticas SET Pulsaciones_Botón=Pulsaciones_Botón+1 WHERE `DNI`=DN1;
 elseif Ganador=1 then
 UPDATE estadisticas SET Pulsaciones_Botón=Pulsaciones_Botón+1 WHERE `DNI`=DN1;
 UPDATE estadisticas SET Pulsaciones_Ganadoras=Pulsaciones_Ganadoras+1 WHERE `DNI`=DN1;
+update saldo set Saldo_Ganado=Saldo_Ganado+Ganado WHERE `DNI`=DN1;
+update saldo set Saldo_Total=Saldo_Total+Ganado WHERE `DNI`=DN1;
 end if;
 END//
 DELIMITER ;
