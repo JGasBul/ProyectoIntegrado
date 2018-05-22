0059faa1e6021640644755041ef430aef73f2422
LAPTOP-VDKPLSA0\Oscar <oscarvillalba939@gmail.com>
2018-05-22 19:37:41 +0200

    Chances

--- Lenguaje de marcas/Web Proyecto Integrado/insertar_cliente.php
+++ Lenguaje de marcas/Web Proyecto Integrado/insertar_cliente.php
@@ -3,27 +3,30 @@
 
 require ("usarGestion.php");
 
-$Nombre=$_REQUEST['Nombre'];
-$Primer_apellido=$_REQUEST['Primer apellido'];
-$Segundo_apellido=$_REQUEST['Segundo apellido'];
-
-$Contraseña=$_REQUEST['Contraseña'];
-$Email=$_REQUEST['E-mail'];
-$Username=$_REQUEST['Username'];
 $DNI=$_REQUEST['DNI'];
+$Nombre=$_REQUEST['Nombre'];
+$Primer_apellido= $_REQUEST['Primer_apellido'];
+$Segundo_apellido=$_REQUEST['Segundo_Apellido'];
+$Username=$_REQUEST['Username'];
 $Teléfono=$_REQUEST['Teléfono'];
+$Contraseña=$_REQUEST['Contraseña'];
+$Email=$_REQUEST['Email'];
+$Tarjeta_Credito=$_REQUEST['Tarjeta_Credito'];
 
 
 
-$consulta ="INSERT INTO usuarios (DNI,Nombre,Primer apellido,Segundo apellido,Contraseña,E-mail,Username,Teléfono) VALUES ('$DNI', '$Primer_apellido', '$Segundo_apellido', '$Contraseña', '$Email', '$Username', '$Teléfono');";
-echo ("La instruccion es: $consulta <br>");
+
+
+
+$consulta ="INSERT INTO usuarios (DNI,Nombre,1ºApellido,2ºApellido,UserName,Telefono,Email,Contraseña,Tarjeta_Credito) VALUES ('$DNI','$Nombre', '$Primer_apellido', '$Segundo_apellido', '$Username', '$Teléfono', '$Email','$Contraseña','$Tarjeta_Credito');";
+ ("La instruccion es: $consulta <br>");
  
 if (!$resultado = $mysqli->query($consulta))
 {
-  echo ("Lo sentimos la instrucción de BBDD ha fallado debido a: <br>");
-  echo ("Query: $consulta <br>");
-  echo ("Error_numero: ". $mysqli->errno . "<br>");
-  echo ("Error: ". $mysqli->error . "<br>");
+ ("Lo sentimos la instrucción de BBDD ha fallado debido a: <br>");
+ ("Query: $consulta <br>");
+ ("Error_numero: ". $mysqli->errno . "<br>");
+ ("Error: ". $mysqli->error . "<br>");
   exit;
 }
 else echo ("Se ha grabado el registro");
--- Lenguaje de marcas/Web Proyecto Integrado/usarGestion.php
+++ Lenguaje de marcas/Web Proyecto Integrado/usarGestion.php
@@ -1,15 +1,15 @@
 <?php
-$servidor = "ec2-54-198-174-97.compute-1.amazonaws.com";
+$servidor = "34.228.255.166";
 $usuario = "tragaperras";
 $clave ="1234";
 
 $mysqli = @mysqli_connect($servidor,$usuario,$clave);
-if (mysqli_connect_errno($mysqli)){
-	echo ("Fallo al conectar a Mysql: ".mysqli_connect_errno($mysqli). " ".
-		mysqli_connect_error($mysqli));
-	die ("NO SE PUDO REALIZAR LA CONEXIÓN");
-}
-else echo ("La conexión se ha producido OK");
+#if (mysqli_connect_errno($mysqli)){
+#	echo ("Fallo al conectar a Mysql: ".mysqli_connect_errno($mysqli). " ".
+#		mysqli_connect_error($mysqli));
+#	die ("NO SE PUDO REALIZAR LA CONEXIÃ“N");
+#}
+#else echo ("La conexiÃ³n se ha producido OK");
 
 $basedatos = "tragaperras";
 mysqli_select_db($mysqli,$basedatos);
